879e00ed016e12dd56fcc01d28787260c4c04316
Juna <115675811+junakim0118@users.noreply.github.com>
2024-09-28 16:09:52 -0400

    closet multiple images

--- maeve/src/Home.js
+++ maeve/src/Home.js
@@ -6,13 +6,13 @@ import maeveLogo from './assets/maevelogo.png';
 
 function Home() {
   return (
-    <div className="w-screen h-screen bg-cover bg-center flex justify-center items-center text-white"
+    <div class="w-screen h-screen bg-cover bg-center flex justify-center items-center text-white"
     style={{ backgroundImage: `url(${homeBg})` }}>
       <Link to="/Closet">
       <img 
           src={maeveLogo} 
           alt="Clickable" 
-          className="w-64 h-auto cursor-pointer"
+          class="w-64 h-auto cursor-pointer"
         />
       </Link>
     </div>
