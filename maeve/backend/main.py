672ebec95b8618901b318f3480c29aa2ff0b5520
Gloria W. <73373391+gloriawng@users.noreply.github.com>
2024-09-29 08:32:17 -0400

    delete gloria's previous main

--- maeve/backend/main.py
+++ maeve/backend/main.py
@@ -1,15 +1,18 @@
-from fastapi import FastAPI
+# from fastapi import Depends 
+# from propelauth_fastapi import User
 
-app = FastAPI()
+# auth = init_auth("https://195023487.propelauthtest.com", "4f38d5f94faa3e3bc186e2758d967d5684b700f56cee703a41c4e2b9dca8fc9e2cd445d68d1faea8532091a3cac1d8bc")
 
-from propelauth_fastapi import init_auth
+# @app.get("/userClothing/")
+# async def get_user_clothing(user: User = Depends(auth.get_current_user)):
+#     return user.clothes  # Return the clothing items associated with the user
 
 
-from fastapi import Depends 
-from propelauth_fastapi import User
+# IF WE WANTED TO IMPLEMENT ACCESS TOKENS: 
+# from fastapi import FastAPI
+# app = FastAPI()
+# from propelauth_fastapi import init_auth
 
-auth = init_auth("https://195023487.propelauthtest.com", "4f38d5f94faa3e3bc186e2758d967d5684b700f56cee703a41c4e2b9dca8fc9e2cd445d68d1faea8532091a3cac1d8bc")
-
-@app.get("/api/whoami")
-async def root(current_user: User = Depends(auth.require_user)):
-    return {"user_id": f"{current_user.user_id}"}
+# @app.get("/api/whoami")
+# async def root(current_user: User = Depends(auth.require_user)):
+#     return {"user_id": f"{current_user.user_id}"}
